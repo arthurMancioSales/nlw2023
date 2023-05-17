ce9f952b6e2bf50e715c799bf52619a32eed8c79
arthurMancioSales <arthur_mancio@outlook.com>
2023-05-17 16:38:39 -0300

    Add: conteúdo aula 01 adicionado

---
+++ web/src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+        <>
+            <h1 className="m-4 rounded-md bg-cyan-100 p-12  text-3xl font-bold">
+                Aula 01
+            </h1>
+        </>
+    );
+}
